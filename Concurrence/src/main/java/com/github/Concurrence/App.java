96a75eecccc8ccf5151f27bbdf180141205713ee
david5201 <306184384@qq.com>
2017-12-20 10:49:57 +0800

    code modify test

--- Concurrence/src/main/java/com/github/Concurrence/App.java
+++ Concurrence/src/main/java/com/github/Concurrence/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello git ,i try to use IDE to pull my project"
+        		+ "i wish everything goes well" );
     }
 }
